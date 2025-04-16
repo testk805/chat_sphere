4adccdf5c7ea12ed358074e96a341a8152fd4132
testk805 <testk805@gmail.com>
2025-04-16 14:27:18 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql2");
 
 var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "chat_sphere",
+  host: "bn4a7py0a8uurhaxofah-mysql.services.clever-cloud.com",
+  user: "ujeqfubo3vwlaios",
+  password: "QwWhQ3tysjHhbC9mn01k",
+  database: "bn4a7py0a8uurhaxofah",
 });
 
 db.connect(function (error) {
