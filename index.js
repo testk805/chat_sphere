e4b14aeaf10e16ef3e290a07fa312e01165a2ae5
testk805 <testk805@gmail.com>
2025-04-28 17:30:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,54 +57,118 @@ const express = require("express");
 app.use("/profile", express.static(path.join(__dirname, "profile")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
  
- const onlineUsers = new Map();
- 
- io.on("connection", (socket) => {
-   socket.on("userOnline", (userId) => {
-     onlineUsers.set(userId, socket.id);
-     io.emit("updateOnlineUsers", Array.from(onlineUsers.keys()));
-   });
-   socket.emit("socketId", socket.id);
-   // 📩 Handle Messages
-   socket.on("sendMessage", (message) => {
-     io.emit("newMessage", message);
-     io.emit("refreshData", {
-       senderId: message.senderId,
-       receiverId: message.receiverId,
-     });
-   });
- 
-   // ✍️ Handle Typing Status
-   socket.on("typing", ({ senderId, receiverId }) => {
-     io.emit("typing", { senderId, receiverId });
-   });
- 
-   socket.on("stopTyping", ({ senderId, receiverId }) => {
-     io.emit("stopTyping", { senderId, receiverId });
-   });
- 
-   socket.on("callUser", (data) => {
-     io.to(data.userToCall).emit("callIncoming", {
-       signal: data.signalData,
-       from: data.from,
-     });
-   });
- 
-   socket.on("answerCall", (data) => {
-     io.to(data.to).emit("callAccepted", data.signal);
-   });
- 
-   socket.on("disconnect", () => {
-     let disconnectedUserId = [...onlineUsers.entries()].find(
-       ([_, sid]) => sid === socket.id
-     )?.[0];
- 
-     if (disconnectedUserId) {
-       onlineUsers.delete(disconnectedUserId);
-       io.emit("updateOnlineUsers", Array.from(onlineUsers.keys()));
-     }
-   });
- });
+const onlineUsers = new Map();
+const peerToSocketMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.emit("socketId", socket.id);
+
+  socket.on("userOnline", (userId) => {
+    onlineUsers.set(userId, socket.id);
+    io.emit("updateOnlineUsers", Array.from(onlineUsers.keys()));
+    console.log(`User ${userId} is online, socket ID: ${socket.id}`);
+  });
+
+  socket.on("setPeerId", (peerId) => {
+    peerToSocketMap.set(peerId, socket.id);
+    console.log("Mapped peer ID:", peerId, "to socket ID:", socket.id);
+  });
+
+  socket.on("sendMessage", (message) => {
+    io.emit("newMessage", message);
+    io.emit("refreshData", {
+      senderId: message.senderId,
+      receiverId: message.receiverId,
+    });
+  });
+
+  socket.on("typing", ({ senderId, receiverId }) => {
+    io.emit("typing", { senderId, receiverId });
+  });
+
+  socket.on("stopTyping", ({ senderId, receiverId }) => {
+    io.emit("stopTyping", { senderId, receiverId });
+  });
+
+  socket.on("callUser", (data) => {
+    console.log(`Call initiated from ${data.from} to ${data.userToCall}`);
+    const socketId = peerToSocketMap.get(data.userToCall);
+    if (socketId) {
+      io.to(socketId).emit("callIncoming", {
+        signal: data.signalData,
+        from: data.from,
+      });
+      console.log(`Emitted callIncoming to socket ID: ${socketId}`);
+    } else {
+      console.log(`No socket found for peer ID: ${data.userToCall}`);
+    }
+  });
+
+  socket.on("answerCall", (data) => {
+    console.log(`Call answered, sending to ${data.to}`);
+    const socketId = peerToSocketMap.get(data.to);
+    if (socketId) {
+      io.to(socketId).emit("callAccepted", data.signal);
+      console.log(`Emitted callAccepted to socket ID: ${socketId}`);
+    } else {
+      console.log(`No socket found for peer ID: ${data.to}`);
+    }
+  });
+
+  socket.on("rejectCall", (data) => {
+    const socketId = peerToSocketMap.get(data.to);
+    console.log("Received rejectCall for peer ID:", data.to, "mapped to socket ID:", socketId);
+    if (socketId) {
+      io.to(socketId).emit("callRejected");
+      console.log("Emitted callRejected to:", socketId);
+    } else {
+      console.log("No socket found for peer ID:", data.to);
+    }
+  });
+
+  socket.on("cancelCall", (data) => {
+    const socketId = peerToSocketMap.get(data.to);
+    console.log("Received cancelCall for peer ID:", data.to, "mapped to socket ID:", socketId);
+    if (socketId) {
+      io.to(socketId).emit("callCancelled");
+      console.log("Emitted callCancelled to:", socketId);
+    } else {
+      console.log("No socket found for peer ID:", data.to);
+    }
+  });
+
+  socket.on("endCall", (data) => {
+    const socketId = peerToSocketMap.get(data.to);
+    console.log("Received endCall for peer ID:", data.to, "mapped to socket ID:", socketId);
+    if (socketId) {
+      io.to(socketId).emit("endCall");
+      console.log("Emitted endCall to:", socketId);
+    } else {
+      console.log("No socket found for peer ID:", data.to);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    let disconnectedUserId = [...onlineUsers.entries()].find(
+      ([_, sid]) => sid === socket.id
+    )?.[0];
+
+    if (disconnectedUserId) {
+      onlineUsers.delete(disconnectedUserId);
+    }
+
+    for (let [peerId, socketId] of peerToSocketMap.entries()) {
+      if (socketId === socket.id) {
+        peerToSocketMap.delete(peerId);
+      }
+    }
+
+    io.emit("updateOnlineUsers", Array.from(onlineUsers.keys()));
+    console.log("Client disconnected:", socket.id);
+  });
+});
  
  // Routes
  const loginRoutes = require("./routes/login");
